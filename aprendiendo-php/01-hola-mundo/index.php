197671f59fd3e9f0b7f8f5769f197085586b28d8
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-06-28 23:45:23 -0500

    01. 1st Hello friend

---
+++ aprendiendo-php/01-hola-mundo/index.php
@@ -0,0 +1,11 @@
+<h1>Hola amigo...</h1>
+
+<?php
+// Apertura de la etiqueta PHP
+// Todo código PHP deberá ir dentro de esta etiqueta
+    
+    echo '<h2>Hola amigo con PHP</h2>'; // Con echo se imprime en pantalla los escrito dentro de las comillas (simples, recomendadas)
+
+// Cierre de la etiqueta PHP
+?>
+

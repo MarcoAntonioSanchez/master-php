dd65bb1d1e47befaf031eb2c6116c9e056608a93
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-07-02 11:09:55 -0500

    06. variables and numbers

--- aprendiendo-php/03-variables/index.php
+++ aprendiendo-php/03-variables/index.php
@@ -1,7 +1,31 @@
 <?php
 
-$miPrimerVariable = "Hola amigo...";
+    //Una variable es un contenedor de información
+    //Se puede almacenar cualquier dato dentro de ella
+    //Declarando una variable con un valor tipo string
+    $miPrimerVariable = "Hola amigo...";
 
-echo "$miPrimerVariable";
+    //Imprimiendo en pantalla una variable
+    echo $miPrimerVariable;
+
+    //Concatenación de variables
+    echo '<h1>'.$miPrimerVariable.'</h1>';
+
+    //Variable de tipo numerico
+    $numero = 7;
+    //Segunda variable, mismo nombre pero diferente valor
+    $numero = 3;
+
+    //Variable de tipo booleano
+    $verdadero = true;
+    
+    //Concatenación de etiqueta br con variable de tipo numerico
+    echo $numero.'<br>';
+    
+    //Se declara una 3er variable numerica, mismo nombre distinto valor
+    $numero = 1;
+    
+    //Se imprime por 2da vez la variable numerica
+    echo $numero;
 
 ?>

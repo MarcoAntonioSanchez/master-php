fa7acd7abb01fd82eac06c5a9bb2e0ffde111db4
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-07-02 15:48:16 -0500

    09. Understanding integer values and some basic usage on them

--- aprendiendo-php/04-tipos/index.php
+++ aprendiendo-php/04-tipos/index.php
@@ -24,6 +24,35 @@ echo gettype($texto);
 
 //Una variable sin valor inicializado automáticamente es igual a null
 $nula = null;
-echo gettype($nula)
+echo gettype($nula);
+
+//Los nombres de la variables no pueden contener caracteres especiales
+// EJEMPLO: $variable_uno | $variableUno
+
+$miNombre = "Marco A. Sánchez";
+
+//var_dump Nos permite ver a detalle las caracteristicas de la variable pasada
+//por parametro, como la ruta del documento, su posición y numero de linea,
+//valor y largo del valor, en el caso de una variable:
+var_dump($miNombre);
+
+$misNombres[] = "Marco";
+
+//En el casi de arreglos, var_dump nos mostrará aún más información
+var_dump($misNombres);
+
+//Con el uso de comillas dobles se pueden agregar variables dentro de cadenas
+//de texto como en el siguiente ejemplo:
+$numeroLoteria = 33;
+$textoEjemplo = "Mi número de lotería es: $numeroLoteria";
+echo $textoEjemplo;
+
+//Un salto de línea como el del siguiente ejemplo, solo aplica si es mostrado
+//por consola:
+$textoConSaltoDeLinea = 'Yo soy la segunda parte.';
+echo "Yo soy la primer parte \n$textoConSaltoDeLinea";
+
+$comillaDoble = 'Hola soy un texto /" y yo otro /" ';
+echo $comillaDoble;
 
 ?>

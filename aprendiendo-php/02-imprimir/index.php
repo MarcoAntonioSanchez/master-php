e5b6176ce7a86647354cd8ea21a9b8338b59eced
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-07-02 10:49:14 -0500

    04. Intro to concatenation

--- aprendiendo-php/02-imprimir/index.php
+++ aprendiendo-php/02-imprimir/index.php
@@ -10,16 +10,22 @@
     <h1>Master en PHP</h1>
     <?php
 
+    //Se pueden usar tantos echo como se requiera, no tienen limites.
+    //Con echo se pueden imprimir etiquetas de HTML
     echo "<h3>Listado de videojuegos:</h3>";
     echo "<ul>
         <li>GTA</li>
         <li>Smash Bros</li>
         <li>Resident Evil</li>
         <li>Metalslug</li>
-        </ul>";
-
+        </ul>"; 
+    
+    //La concatenación esta limitada a las comillas simples, en PHP 8.0.13 no se puede concatenar con comillas dobles.
         echo '<p>Esta es toda'.' - '.'Lista de juegos</p>';
 
 ?>
+    
+    <?="Abreviación con bloque echo de PHP"?>
+   
 </body>
 </html>

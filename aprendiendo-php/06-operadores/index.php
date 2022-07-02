8a12d3291a06870ef1ccc6e7a3b2432714435440
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-07-02 16:09:49 -0500

    12. Intro to aritmetic operators

---
+++ aprendiendo-php/06-operadores/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    //Operadores aritmeticos
+
+    //Se declaran 2 variables numericas con valores inicializados:
+    $numero1 = 55;
+    $numero2 = 33;
+
+    //Se imprime la suma de las variables anteriormente declaradas:
+    echo 'suma: '.($numero1+$numero2).'<br>';
+
+    //Se imprime la resta de las variables numero 1 y 2 respectivamente:
+    echo 'resta: '.($numero1-$numero2).'<br>';
+
+    //Se imprime la multiplicacion de las variables numero 1 y 2: 
+    echo 'multiplicación: '.($numero1*$numero2).'<br>';
+    
+    //Se imprime la división de las variables numero 1 y 2:
+    echo 'división: '.($numero1/$numero2).'<br>';
+    
+    //Se imprime el restante de la división entre las variables numero 1 y 2:
+    echo 'resto: '.($numero1%$numero2).'<br>';
+    
+?>

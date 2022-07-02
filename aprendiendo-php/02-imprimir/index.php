12c315c5bab1edf6f2bc16e11ca1caaa44c143e4
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-07-01 21:11:05 -0500

    02. imprimiendo 02 added

---
+++ aprendiendo-php/02-imprimir/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imprimir Por Pantalla - Master PHP</title>
+</head>
+<body>
+    <h1>Master en PHP</h1>
+    <?php
+
+    echo "<h3>Listado de videojuegos:</h3>";
+    echo "<ul>
+        <li>GTA</li>
+        <li>Smash Bros</li>
+        <li>Resident Evil</li>
+        <li>Metalslug</li>
+        </ul>";
+
+        echo '<p>Esta es toda'.' - '.'Lista de juegos</p>';
+
+?>
+</body>
+</html>

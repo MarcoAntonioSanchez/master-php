d65336c8e47b3c50fe5dfee5e9f6d14b14dd8a6d
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-07-08 00:20:07 -0500

    27. Using foor loops to GET data using the url

---
+++ aprendiendo-php/10-bucle-for/index.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Bucle for
+ 
+for(variable contador, condicion, actualizando contador){
+    bloque de instrucciones
+}
+
+
+$resultado = 0;
+
+for($i = 0; $i <= 100; $i++){
+    $resultado += $i;
+    echo "<p>$resultado</p>";
+}
+
+echo "<h1>El resultado es: $resultado</h1>";
+
+echo "<br>"
+*/
+
+if(isset($_GET['numero'])){
+    
+//(int) Para cambiar el tipo de dato de string a entero o int
+    $numero = (int)$_GET['numero'];
+}else{
+    $numero = 1;
+}
+
+//Ejemplo de while usando el metodo GET para mostrar la tabla de multiplicación
+//correspondiente. URL: /?numero=8
+echo "<h1>Tabla de multiplicar del número $numero</h1>";
+
+for($contador = 1; $contador <= 10; $contador++){
+    echo "$numero x $contador = ".($numero*$contador)."<br/>";
+}
+
+?>

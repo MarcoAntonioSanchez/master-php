47b2d8f0c65c0ebb031c30e294c45f6f807b38ac
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-08-07 18:38:05 -0500

    52. Creating new .php file for the header for including it on index.php

---
+++ aprendiendo-php/13-includes/includes/header.php
@@ -0,0 +1,9 @@
+    <div>
+    <h1>Includes en PHP</h1>
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="index.php">Sobre mi</a></li>
+        <li><a href="index.php">Contacto</a></li>
+    </ul>
+    </div>
+    <hr>

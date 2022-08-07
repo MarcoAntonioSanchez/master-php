df389741512f65647f98113ed2e249863876de59
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-08-07 18:33:42 -0500

    51. Starting new folder - PHP includes with 3 .php pages

---
+++ aprendiendo-php/13-includes/sobre-mi.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Includes en PHP</title>
+</head>
+<body>
+    <!-- Header -->
+    <div>
+    <h1>Includes en PHP</h1>
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="index.php">Sobre mi</a></li>
+        <li><a href="index.php">Contacto</a></li>
+    </ul>
+    </div>
+    <hr>
+    <!-- Content -->
+    <div>
+        <h2>Esta es la página sobre mi</h2>
+        <p>Texto de prueba de la página sobre mi</p>
+    </div>
+        <hr>
+    <!-- Footer -->
+    <div>
+        <footer>
+            Todos los derechos reservados &copy; Marco A. Sánchez <?=date('Y')?>
+        </footer>
+    </div>
+</body>
+</html>

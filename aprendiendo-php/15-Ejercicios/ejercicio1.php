78a81b4ddba11d2b9b500ef3efdc0feb90277d25
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-08-07 20:45:57 -0500

    65. Starting the folder - .php page for exercises

---
+++ aprendiendo-php/15-Ejercicios/ejercicio1.php
@@ -0,0 +1,55 @@
+<?php
+
+/* 
+ * Ejercicio 1:
+ * Hacer un programa en PHP que tenga un arreglo con 8 numeros enteros
+ * y que haga lo siguiente:
+ * 1. Recorrerlo y mostrarlo
+ * 2. Ordenarlo y mostrarlo
+ * 3. Mostrar longitud
+ * 4. Buscar algun elemento dentro del arreglo
+ * */
+
+echo "<h3>Arreglo:</h3>";
+echo "<ul>
+        <li>Pikachu</li>
+        <li>Charmander</li>
+        <li>Bulbasaur</li>
+        <li>Squirtle</li>
+        <li>Butterfly</li>
+        <li>Toggepi</li>
+     </ul>";
+echo "<br/>";
+
+$pokemones = ['Pikachu', 'Charmander', 'Bulbasaur', 'Squirtle', 'Butterfly', 'Toggepi'];
+var_dump($pokemones);
+
+echo "<br/><hr>";
+
+echo "<h3>Recorrerlo y mostrarlo:</h3>";
+echo "<br/>";
+
+
+
+echo "<br/><hr>";
+
+echo "<h3>Ordenarlo y mostrarlo:</h3>";
+echo "<br/>";
+
+
+
+echo "<br/><hr>";
+
+echo "<h3>Mostrar longitud:</h3>";
+echo "<br/>";
+
+
+
+echo "<br/><hr>";
+
+echo "<h3>Buscar elemento dentro del arreglo:</h3>";
+echo "<br/>";
+
+
+
+echo "<br/><hr>";

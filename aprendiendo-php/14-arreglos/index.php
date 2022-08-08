3bd5b2ea44464d4d72e4710d92916f7a6afafe65
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-08-07 19:29:31 -0500

    59. Reading through arrays using for and foreach loops

--- aprendiendo-php/14-arreglos/index.php
+++ aprendiendo-php/14-arreglos/index.php
@@ -5,9 +5,37 @@ Arreglos:
  * Un arreglo (array) es una colección o conjunto de datos bajo 1 único nombre
  * Para acceder a esos valores se puede usar un identificador alfanumerico
 */
+echo "<h3>Ejemplo 1</h3>";
+echo "<p>Recorriendo arreglos por posición:</p>";
 
 $pelicula = "Batmna";
 $peliculas = array('Batman', 'Spiderman', 'El señor de los anillos');
 $cantantes = ['2pac', 'Eminem', 'Snoop Dogg'];
 
 echo $peliculas[0];
+echo "<hr>";
+
+//Recorrer con for
+echo "<h3>Ejemplo 2</h3>";
+echo "<p>Recorriendo arreglos por posición:</p>";
+
+echo "<h1>Listado de peliculas:</h1>";
+echo "<ul>";
+for($i = 0; $i < count($peliculas); $i++){
+    echo "<li>".$peliculas[$i]."</li>";
+}
+echo "</ul>";
+echo "<hr>";
+
+echo "<h3>Ejemplo 3</h3>";
+echo "<p>For each:</p>";
+
+echo "<h1>Listado de cantantes</h1>";
+echo "<ul>";
+
+foreach ($cantantes as $cantante) {
+    echo "<li>".$cantante."</li>";
+}
+echo "</ul>";
+
+echo "<hr>";

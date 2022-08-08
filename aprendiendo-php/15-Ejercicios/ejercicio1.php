58eff1572d4fe81c0634f74d04a736500211e6a8
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-08-07 21:33:20 -0500

    66. Completing the exercises with the references from the guide

--- aprendiendo-php/15-Ejercicios/ejercicio1.php
+++ aprendiendo-php/15-Ejercicios/ejercicio1.php
@@ -7,49 +7,69 @@
  * 1. Recorrerlo y mostrarlo
  * 2. Ordenarlo y mostrarlo
  * 3. Mostrar longitud
- * 4. Buscar algun elemento dentro del arreglo
+ * 4. Buscar algun elemento dentro del arreglo (buscar por el parametro que me lleve por la url)
  * */
 
+//Funciones:
+function mostrarArray($pokemones){
+    $resultado = "";
+    foreach ($pokemones as $pokemon) {
+    $resultado .= $pokemon."<br/>";
+    }
+    return $resultado;
+}
+
 echo "<h3>Arreglo:</h3>";
-echo "<ul>
-        <li>Pikachu</li>
-        <li>Charmander</li>
-        <li>Bulbasaur</li>
-        <li>Squirtle</li>
-        <li>Butterfly</li>
-        <li>Toggepi</li>
-     </ul>";
-echo "<br/>";
 
 $pokemones = ['Pikachu', 'Charmander', 'Bulbasaur', 'Squirtle', 'Butterfly', 'Toggepi'];
 var_dump($pokemones);
+echo "<br/>";
 
 echo "<br/><hr>";
 
 echo "<h3>Recorrerlo y mostrarlo:</h3>";
 echo "<br/>";
 
+echo "<h4>Opción 1:</h4>";
+for($i = 0; $i < count($pokemones); $i++){
+    echo "<li>".$pokemones[$i]."</li>";
+}
 
+echo "<br/>";
+
+echo "<h4>Opción 2:</h4>";
+echo mostrarArray($pokemones);
 
 echo "<br/><hr>";
 
 echo "<h3>Ordenarlo y mostrarlo:</h3>";
 echo "<br/>";
 
-
+sort($pokemones);
+echo mostrarArray($pokemones);
 
 echo "<br/><hr>";
 
 echo "<h3>Mostrar longitud:</h3>";
 echo "<br/>";
 
-
+echo count($pokemones);
 
 echo "<br/><hr>";
 
 echo "<h3>Buscar elemento dentro del arreglo:</h3>";
 echo "<br/>";
 
-
+if(isset($_GET['pokemon'])){
+    $busqueda = $_GET['pokemon'];
+    
+    $search = array_search($busqueda, $pokemones);
+    
+    if(!empty($search)){
+        echo "El pokemon: existe en el arreglo, en la posición numero: $search";
+    }else{
+        echo "No se encontró el pokemon buscado";
+    }
+}
 
 echo "<br/><hr>";

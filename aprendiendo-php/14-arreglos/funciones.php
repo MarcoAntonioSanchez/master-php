67d883676a711a024b64326ef496a55dc00436da
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-08-07 20:16:10 -0500

    62. Adding the 2 - 3 examples on manipulating elements inside arrays

--- aprendiendo-php/14-arreglos/funciones.php
+++ aprendiendo-php/14-arreglos/funciones.php
@@ -21,7 +21,27 @@ echo "<h3>Ejemplo 2</h3>";
 echo "<p>Más arreglos multidimensionales:</p>";
 
 //Añadir elementos
-
+$cantantes[] = "Costa";
+array_push($cantantes, "Capaz");
+array_pop($cantantes);
+unset($cantantes[2]);
+var_dump($cantantes);
 
 echo "<hr>";
 
+echo "<h3>Ejemplo 3</h3>";
+echo "<p>Arreglos multidimensionales:</p>";
+
+//Aleatorio
+$indice = array_rand($cantantes);
+echo $cantantes[$indice];
+
+echo "<hr>";
+
+echo "<h3>Ejemplo 4</h3>";
+echo "<p>Más arreglos multidimensionales:</p>";
+
+//Dar la vuelta
+var_dump(array_reverse($numeros));
+
+echo "<hr>";

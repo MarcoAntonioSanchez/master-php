e1cd196ff9ad04bd1f0b7a054e01d3d4df937841
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2022-08-07 19:00:28 -0500

    58. Understanding the structure of simple arrays

--- aprendiendo-php/14-arreglos/index.php
+++ aprendiendo-php/14-arreglos/index.php
@@ -1,7 +1,13 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+/*
+Arreglos:
+ * Un arreglo (array) es una colección o conjunto de datos bajo 1 único nombre
+ * Para acceder a esos valores se puede usar un identificador alfanumerico
+*/
 
+$pelicula = "Batmna";
+$peliculas = array('Batman', 'Spiderman', 'El señor de los anillos');
+$cantantes = ['2pac', 'Eminem', 'Snoop Dogg'];
+
+echo $peliculas[0];
